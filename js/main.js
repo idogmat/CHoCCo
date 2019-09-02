091eeb4035a9af4f889cc61f2e08ffd721601505
Evgeny Rodin <idogmat@gmail.com>
2019-09-02 10:26:20 +0300

    +

--- js/main.js
+++ js/main.js
@@ -1,39 +1,54 @@
-//slider
-let slides = document.querySelectorAll('.slider-item'),
-prev = document.querySelector('#arrow-left'),
-next = document.querySelector('#arrow-right'),
-slideIndex = 1;
+window.addEventListener('DOMContentLoaded', function () {
 
-function showSlides(n) {
-    if (n > slides.length) {
+    'use strict';
+
+    // //slider
+    let slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('#arrow-left'),
+        next = document.querySelector('#arrow-right'),
         slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.add('structure-block-visible');
-        slides[i].classList.remove('structure-block-hidden');
-        slides[slideIndex - 1].classList.add('structure-block-hidden');
-        slides[slideIndex - 1].classList.remove('structure-block-visible');
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].classList.add('structure-block-visible');
+            slides[i].classList.remove('structure-block-hidden');
+            slides[slideIndex - 1].classList.add('structure-block-hidden');
+            slides[slideIndex - 1].classList.remove('structure-block-visible');
             // slides[i].style.display = 'none';
             // slides[slideIndex - 1].style.display = 'flex';
         }
 
     }
-        function plusSlides(n) {
-            showSlides(slideIndex += n);
-        }
-    
-        function currentSlide(n) {
-            showSlides(slideIndex = n);
-        }
-    
-        prev.addEventListener('click', function () {
-            plusSlides(-1);
-        });
-    
-        next.addEventListener('click', function () {
-            plusSlides(1);
-        });
-//slider-show
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function () {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function () {
+        plusSlides(1);
+    });
+
+    //main-menu
+    var clickMenu = document.getElementById('#mini-menu'),
+        tabletMenu = document.getElementById('#menu-tablet');
+
+    clickMenu.addEventListener('click', function (e) {
+        e.preventDefault();
+        tabletMenu.classList.add('structure-block-visible');
+    });
+
+});
